cdea9c90b6dd9dd2ee582f4600fa0364badd9ab1
siseveca79 <simonsebastianvelasquezcarcamo@gmail.com>
2024-02-19 14:47:45 -0700

    quinto envio

--- Assets - Ricomida/js/script.js
+++ Assets - Ricomida/js/script.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
-  // Selectores de clase: Ocultar y mostrar contenido de tarjetas "card"
-  $('.card').click(function() {
+  // Evento click para mostrar alerta al hacer clic en el botón "Enviar por correo"
+  $('#enviarCorreo').on('click', function() {
+      alert("El correo fue enviado correctamente...");
+  });
+
+  // Evento click para mostrar alerta al hacer clic en el botón "Añadir a favoritos"
+  $('#favorito').on('click', function() {
+      alert("Receta añadida a favoritos");
+  });
+
+  // Evento dblclick para cambiar el color del texto de los títulos "INGREDIENTES" y "PREPARACIÓN"
+  $('h3').on('dblclick', function() {
+      $(this).css('color', 'red');
+  });
+
+  // Evento click para toggle el contenido de las tarjetas "card"
+  $('.card').on('click', function() {
       $(this).find('.card-body').toggle();
   });
-});
 
-
-$(document).ready(function() {
+  // Inicialización del carousel de Bootstrap
   $('.carousel').carousel();
 });
