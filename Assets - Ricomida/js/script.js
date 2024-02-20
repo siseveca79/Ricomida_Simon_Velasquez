c190574024dc7a7de9c056e13d17a82d610499e1
siseveca79 <simonsebastianvelasquezcarcamo@gmail.com>
2024-02-19 18:20:40 -0700

    noveno envio

--- Assets - Ricomida/js/script.js
+++ Assets - Ricomida/js/script.js
@@ -13,17 +13,3 @@
     new bootstrap.Carousel(carousel);
   });
 });
-
-
-document.addEventListener("DOMContentLoaded", function() {
-  var navbarToggler = document.querySelector(".navbar-toggler");
-  var navbarCollapse = document.querySelector(".navbar-collapse");
-
-  navbarToggler.addEventListener("click", function() {
-      if (navbarCollapse.classList.contains("show")) {
-          navbarCollapse.classList.remove("show");
-      } else {
-          navbarCollapse.classList.add("show");
-      }
-  });
-});
